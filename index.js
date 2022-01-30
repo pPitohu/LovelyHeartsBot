f12f81113dfb29045a28911121dc4c02d38dfb90
pPitohu <59769228+pPitohu@users.noreply.github.com>
2022-01-31 02:31:53 +0300

    s

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ const app = express();
 
 const bot = new Telegraf(process.env.LovelyHeartsBOT_TOKEN);
 const SurpriseNotifier = new Telegraf(process.env.SurpriseNotifierBOT_TOKEN);
+
+bot.setWebhook(
+    'https://lovelyhearts-bot.herokuapp.com/' +
+        process.env.LovelyHeartsBOT_TOKEN
+);
+SurpriseNotifierBOT_TOKEN.setWebhook(
+    'https://lovelyhearts-bot.herokuapp.com/' +
+        process.env.SurpriseNotifierBOT_TOKEN
+);
+
 bot.use(session());
 SurpriseNotifier.use(session());
 
@@ -258,11 +268,21 @@ mongoose.connect(
     () => console.log('connected to database')
 );
 
-bot.launch({
-    webhook: {
-        domain: process.env.DOMAIN,
-        port: process.env.PORT,
-    },
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Hello from the Bot API.' });
+});
+// TELEGRAM WEBHOOK - https://core.telegram.org/bots/api#setwebhook
+app.post(`/${process.env.TELEGRAM_TOKEN}`, (req, res) => {
+    bot.processUpdate(req.body);
+    res.status(200).json({ message: 'ok' });
+});
+
+app.listen(port, () => {
+    console.log(`\n\nServer running on port ${port}.\n\n`);
 });
 
 process.once('SIGINT', () => bot.stop('SIGINT'));

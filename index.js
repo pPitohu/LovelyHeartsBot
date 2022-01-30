a25af5c58ee187f9f14bf4afd48ce1b80422a1fc
pPitohu <59769228+pPitohu@users.noreply.github.com>
2022-01-30 22:49:50 +0300

    p

--- index.js
+++ index.js
@@ -68,14 +68,11 @@ SurpriseNotifier.start(async (ctx) => {
 });
 
 bot.start(async (ctx) => {
-    let text = `
-    <b>Привет, ${
-        ctx.from.first_name ? ctx.from.first_name : 'Незнакомец'
-    }! ♥️</b>
-    `;
     await ctx.reply(
-        text +
-            `
+        `
+        <b>Привет, ${
+            ctx.from.first_name ? ctx.from.first_name : 'Незнакомец'
+        }! ♥️</b>
         Этот бот поможет тебе сделать приятный сюрприз твоему другу или просто близкому человеку.
         Чтобы узнать, как это сделать, держи команду: /how <i>(нажми)</i>
         `.replaceAll('  ', ''),
